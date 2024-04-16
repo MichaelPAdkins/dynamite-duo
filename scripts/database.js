84d483711ffd7f75c7b5de6dcbb2fdd1620468f2
Michael Adkins <michaelpauladkins@gmail.com>
2024-04-16 11:45:28 -0400

    created villains file in script folder

--- scripts/database.js
+++ scripts/database.js
@@ -13,9 +13,40 @@ const database = {
     ]
 }
 
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "ADHD-Man",
+            power: "Vitality sucking monologues"
+        },
+        {
+            id: 2,
+            name: "Manufacturer's Coupon",
+            power: "Manufactures conditions capable of altering reality"
+        }
+    ]
+}
+
 //I feel like I really like the line breaks between different components in the code.
 //Not sure if that's a best practice, however.
 
 export const getHeroes = () => {
     return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
 }
